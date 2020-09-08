b6ebb7c99385a06215b0572aaca894e7bdeabd23
ShaharShi <Shaharshitrit01@gmail.com>
2020-09-08 12:50:55 +0300

    Movie data presentation in movie page

--- src/components/pages/movie-page/index.tsx
+++ src/components/pages/movie-page/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import styles from './styles.module.css'
 import { useParams, useHistory } from "react-router-dom"
 import Axios from "axios";
 import Button from "react-bootstrap/esm/Button";
@@ -29,26 +30,44 @@ export default function MoviePage() {
     console.log(currentMovieData);
     
     if(!Object.keys(currentMovieData).length) return < Spinner animation="border" role="status"> </Spinner>
-    return (<div>
-    <h1> {currentMovieData.title}</h1>
-        <h2> Movie id: {movieId} </h2>
-        <Button onClick={() => history.push(`/`)} > Go Back to Movies Page</Button>
+    return (<div className={'row justify-content-around mt-5'}>
+        <div className={styles.leftMovieDetails}>
+            <img src={currentMovieData.poster} height={'400px'} width={'300px'}></img>
+            <h4> {currentMovieData.title}</h4>
+            <p>{currentMovieData.type} ({currentMovieData.year})
+                <p>{currentMovieData.genre}</p>
+            </p>
+        </div>
+        <div className={`row ${styles.rightMovieDetails}`}>
+            <div className={styles.moviePlot}>
+                <p>{currentMovieData.plot}</p>
+            </div>
+            <div className={styles.movieDetails}>
+                <span><b>Language:</b> {currentMovieData.language}</span>
+                <span><b>Country:</b> {currentMovieData.country}</span>
+                <span><b>Released Date:</b> {currentMovieData.released}</span>
+                <span><b>Actors:</b> {currentMovieData.actors}</span>
+                <span><b>IMDb Rating:</b> {currentMovieData.imdbRating}</span>
+            </div>
+            <Button className={'m-auto'} onClick={() => history.push(`/`)}> Go Back to Movies Page</Button>
+        </div>
     </div>)
 }
 
 function mappingMovieData(data: any) {
-    const { Title, Genre, language, plot, Poster, Released, Actors, Type, country, Year, imdbRating } = data
+    const { Title, Genre, Language, Plot, Poster, Released, Actors, Type, Country, Year, imdbRating } = data;
+    const defaultPoster = 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTIM3fqvuBBZVd-tPsR1fgnUVEsPjVzk3iBtg&usqp=CAU'
     return {
         title: Title,
-        genre: Genre, 
-        language: language, 
-        plot: plot, 
-        poster: Poster, 
-        released: Released, 
-        actors: Actors,
-        type: Type,
-        country: country,
-        year: Year,
-        imdbRating: imdbRating
+        year: Year === 'N/A' ? 'No Data' : Year,
+        genre: Genre === 'N/A' ? 'No Genre' : Genre, 
+        language: Language === 'N/A' ? 'No Data' : Language, 
+        plot: Plot === 'N/A' ? 'Nothing to tell about ...' : Plot, 
+        poster: Poster === 'N/A' ? defaultPoster : Poster, 
+        released: Released === 'N/A' ? 'No Data' : Released, 
+        actors: Actors === 'N/A' ? 'No Data' : Actors,
+        type: Type === 'N/A' ? 'No Data' : Type,
+        country: Country === 'N/A' ? 'No Data' : Country,
+        imdbRating: imdbRating === 'N/A' ? 'No Data' : imdbRating
     }
-}
+}   
